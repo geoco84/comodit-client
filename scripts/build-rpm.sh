5b1168268d2699ae379550405b5e7f0cb8a62560
gdethier <gerard.dethier@guardis.com>
2011-10-18 15:33:38 +0200

    Updated RPM build.

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -6,12 +6,19 @@ COMMIT=`git describe --long --match "release*" | awk -F"-" '{print $4}'`
 
 NAME=cortex-client
 TAR_CONTENT="cortex_client templates conf setup.py cortex-client"
+PLATFORMS=(epel-6-i386 fedora-14-i386 fedora-15-i386)
 
 cd `dirname $0`
 cd ..
 
 sed "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${NAME}.spec.template > rpmbuild/SPECS/${NAME}.spec
 sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${NAME}.spec
+sed -i "s/#COMMIT#/${COMMIT}/g" rpmbuild/SPECS/${NAME}.spec
 
 tar -cvzf $HOME/rpmbuild/SOURCES/${NAME}-${VERSION}-${RELEASE}.tar.gz ${TAR_CONTENT}
 rpmbuild -ba rpmbuild/SPECS/${NAME}.spec
+
+for platform in "${platforms[@]}"
+do
+    mock -r ${platform} --rebuild $HOME/rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
+done
--- scripts/deploy-trunk.sh
+++ scripts/deploy-trunk.sh
@@ -1,10 +1,10 @@
-#/bin/bash
+#!/bin/bash
 echo "Building cortex-client from trunk"
 
 cd `dirname $0`
 cd ..
 
-git checkout dev
+git checkout python_api
 git pull
 
 NAME="cortex-client"
@@ -12,6 +12,21 @@ VERSION=`git describe --long --match "release*" | awk -F"-" '{print $2}'`
 RELEASE=`git describe --long --match "release*" | awk -F"-" '{print $3}'`
 
 ./scripts/build-rpm.sh
-/var/www/html/public/deploy-trunk.sh $HOME/rpmbuild/RPMS/noarch/${NAME}-${VERSION}-${RELEASE}.noarch.rpm
-/var/www/html/public/updaterepo.sh
 
+cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-trunk/centos/6.0/noarch/
+cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-trunk/centos/6.0/SRPMS/
+
+cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-trunk/fedora/14/noarch/
+cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-trunk/fedora/14/SRPMS/
+
+cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-trunk/fedora/15/noarch/
+cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-trunk/fedora/15/SRPMS/
+
+cp -n /var/www/html/public/comodit-trunk/centos/6.0/noarch/*.rpm /var/www/html/public/comodit-trunk/centos/6.0/i686/
+cp -n /var/www/html/public/comodit-trunk/centos/6.0/noarch/*.rpm /var/www/html/public/comodit-trunk/centos/6.0/x86_64/
+
+cp -n /var/www/html/public/comodit-trunk/fedora/14/noarch/*.rpm /var/www/html/public/comodit-trunk/fedora/14/i686/
+cp -n /var/www/html/public/comodit-trunk/fedora/14/noarch/*.rpm /var/www/html/public/comodit-trunk/fedora/14/x86_64/
+
+cp -n /var/www/html/public/comodit-trunk/fedora/15/noarch/*.rpm /var/www/html/public/comodit-trunk/fedora/15/i686/
+cp -n /var/www/html/public/comodit-trunk/fedora/15/noarch/*.rpm /var/www/html/public/comodit-trunk/fedora/15/x86_64/
