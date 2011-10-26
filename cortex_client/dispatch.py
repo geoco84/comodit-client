f2e935048670ce8b538415547c8be9c8cc7ac2ad
gdethier <gerard.dethier@guardis.com>
2011-10-26 08:54:21 +0200

    Fixes issue #19. Setting of ownership and permissions when rendering tree may be disabled. Moreover, a more explicit error message is displayed when setting of ownership or permissions fails.

--- cortex_client/dispatch.py
+++ cortex_client/dispatch.py
@@ -36,11 +36,11 @@ from api.exceptions import PythonApiException
 def run(argv):
     # resources
     control.router.register(["users"], UsersController())
-    control.router.register(["pf",  "platforms"], PlatformsController())
-    control.router.register(["app",  "applications"], ApplicationsController())
+    control.router.register(["pf", "platforms"], PlatformsController())
+    control.router.register(["app", "applications"], ApplicationsController())
     control.router.register(["dist", "distributions"], DistributionsController())
-    control.router.register(["org",  "organizations"], OrganizationsController())
-    control.router.register(["env",  "environments"], EnvironmentsController())
+    control.router.register(["org", "organizations"], OrganizationsController())
+    control.router.register(["env", "environments"], EnvironmentsController())
     control.router.register(["host", "hosts"], HostsController())
     control.router.register(["sync"], SyncController())
     control.router.register(["cr", "changes"], ChangesController());
@@ -57,40 +57,43 @@ def _parse(argv):
     parser = optparse.OptionParser(usage)
 
     try:
-        config=Config()
+        config = Config()
     except ConfigException, e:
         print "Configuration error:"
         print e.msg
         exit(-1)
 
-    parser.add_option("-f", "--file", dest="filename", help="input file with a JSON object")
-    parser.add_option("-j", "--json", dest="json",     help="input JSON object via command line")
-    parser.add_option("--raw",        dest="raw",      help="output the raw JSON results", action="store_true", default=False,)
-    parser.add_option("--with-uuid",  dest="uuid",     help="references are UUID instead of paths",  action="store_true", default=False)
-    
-    parser.add_option("--org",        dest="org",      help="Path or UUID of the parent organization (conditioned by --with-uuid)")
-    parser.add_option("--org-path",   dest="org_path", help="Path to the parent organization")
-    parser.add_option("--org-uuid",   dest="org_uuid", help="UUID of the parent organization")
+    parser.add_option("-f", "--file", dest = "filename", help = "input file with a JSON object")
+    parser.add_option("-j", "--json", dest = "json", help = "input JSON object via command line")
+    parser.add_option("--raw", dest = "raw", help = "output the raw JSON results", action = "store_true", default = False,)
+    parser.add_option("--with-uuid", dest = "uuid", help = "references are UUID instead of paths", action = "store_true", default = False)
 
-    parser.add_option("--env",        dest="env",      help="Path or UUID of the parent environment (conditioned by --with-uuid)")
-    parser.add_option("--env-path",   dest="env_path", help="Path to the parent environment")
-    parser.add_option("--env-uuid",   dest="env_uuid", help="UUID of the parent environment")
+    parser.add_option("--org", dest = "org", help = "Path or UUID of the parent organization (conditioned by --with-uuid)")
+    parser.add_option("--org-path", dest = "org_path", help = "Path to the parent organization")
+    parser.add_option("--org-uuid", dest = "org_uuid", help = "UUID of the parent organization")
 
-    parser.add_option("--api",        dest="api",      help="endpoint for the API",      default=config.config["client"]["api"])
-    parser.add_option("--user",       dest="username", help="username on cortex server", default=config.config["client"]["username"])
-    parser.add_option("--pass",       dest="password", help="password on cortex server", default=config.config["client"]["password"])
-    parser.add_option("--templates",  dest="templates_path", help="directory containing JSON templates", default=config.templates_path)
+    parser.add_option("--env", dest = "env", help = "Path or UUID of the parent environment (conditioned by --with-uuid)")
+    parser.add_option("--env-path", dest = "env_path", help = "Path to the parent environment")
+    parser.add_option("--env-uuid", dest = "env_uuid", help = "UUID of the parent environment")
 
-    parser.add_option("--quiet",      dest="verbose",  help="don't print status messages to stdout", action="store_false", default=True)
-    parser.add_option("--force",      dest="force",    help="bypass change management and update everything", action="store_true", default=False)
-    parser.add_option("--debug",      dest="debug",    help="display debug information", action="store_true", default=False)    
-    parser.add_option("--version",    dest="version",  help="display version information", action="store_true", default=False)
+    parser.add_option("--skip-chown", dest = "skip_chown", help = "Path to the parent environment", action = "store_true", default = False)
+    parser.add_option("--skip-chmod", dest = "skip_chmod", help = "UUID of the parent environment", action = "store_true", default = False)
+
+    parser.add_option("--api", dest = "api", help = "endpoint for the API", default = config.config["client"]["api"])
+    parser.add_option("--user", dest = "username", help = "username on cortex server", default = config.config["client"]["username"])
+    parser.add_option("--pass", dest = "password", help = "password on cortex server", default = config.config["client"]["password"])
+    parser.add_option("--templates", dest = "templates_path", help = "directory containing JSON templates", default = config.templates_path)
+
+    parser.add_option("--quiet", dest = "verbose", help = "don't print status messages to stdout", action = "store_false", default = True)
+    parser.add_option("--force", dest = "force", help = "bypass change management and update everything", action = "store_true", default = False)
+    parser.add_option("--debug", dest = "debug", help = "display debug information", action = "store_true", default = False)
+    parser.add_option("--version", dest = "version", help = "display version information", action = "store_true", default = False)
 
     (globals.options, args) = parser.parse_args()
 
     if globals.options.version:
         print "Cortex command line client, version " + VERSION + ", released on " + RELEASE + "."
-        exit(0)  
+        exit(0)
 
     if (len(args) == 0):
         parser.print_help()
@@ -101,35 +104,35 @@ def _parse(argv):
                     globals.options.password)
 
     _dispatch(args[0], args[1:], api)
-    
+
 def _dispatch(resource, args, api):
     options = globals.options
-    
+
     try:
         control.router.dispatch(resource, api, args)
         exit(0)
     except ControllerException as e:
         print e.msg
     except ArgumentException as e:
-        print e.msg     
+        print e.msg
     except ApiException as e:
         print "Error (%s): %s" % (e.code, e.message)
     except PythonApiException as e:
         print e.message
     except NotModifiedException:
-        print "Command was canceled since you did not save the file"       
+        print "Command was canceled since you did not save the file"
     except Exception:
         if options.debug:
             print "Exception in user code."
         else :
             print "Oops, it seems something went wrong (use --debug to learn more)."
 
-    if options.debug:  
+    if options.debug:
         print '-' * 60
-        traceback.print_exc(file=sys.stdout)
+        traceback.print_exc(file = sys.stdout)
         print '-' * 60
     exit(-1)
-        
+
 def print_resources():
     print '''
 Resources:
