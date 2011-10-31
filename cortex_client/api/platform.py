6570831867db0bb4fe7baa7cdd66e380d6ff5ee0
gdethier <gerard.dethier@guardis.com>
2011-10-31 15:04:23 +0100

    Documented platform module.

--- cortex_client/api/platform.py
+++ cortex_client/api/platform.py
@@ -1,8 +1,26 @@
+# coding: utf-8
+"""
+Platform module.
+
+@organization: Guardis
+@copyright: 2011 Guardis SPRL, Liège, Belgium.
+"""
+
 from resource import Resource
 from host import SettingFactory
 
 class Platform(Resource):
+    """
+    Platform's description. A platform is described by a driver and a list of
+    settings.
+    """
     def __init__(self, api = None, json_data = None):
+        """
+        @param api: An access point.
+        @type api: L{CortexApi}
+        @param json_data: A quasi-JSON representation of application's state.
+        @type json_data: dict, list or String
+        """
         super(Platform, self).__init__(json_data)
         if(api):
             self.set_api(api)
@@ -12,28 +30,68 @@ class Platform(Resource):
         self._set_collection(api.get_platform_collection())
 
     def get_driver(self):
+        """
+        Provides platform's driver class name.
+        @return: Driver's class name
+        @rtype: String
+        """
         return self._get_field("driver")
 
-    def set_driver(self, owner):
-        return self._set_field("driver", owner)
+    def set_driver(self, driver):
+        """
+        Sets platform's driver class name.
+        @param driver: Driver's class name
+        @type driver: String
+        """
+        return self._set_field("driver", driver)
 
     def get_owner(self):
+        """
+        Provides platform's owner UUID.
+        @return: A user's UUID
+        @rtype: String
+        """
         return self._get_field("owner")
 
     def set_owner(self, owner):
+        """
+        Sets platform's owner.
+        @param owner: A user's UUID
+        @type owner: String
+        """
         return self._set_field("owner", owner)
 
     def get_settings(self):
+        """
+        Provides platform's settings.
+        @return: A list of settings
+        @rtype: list of L{Setting}
+        """
         return self._get_list_field("settings", SettingFactory())
 
-    def set_settings(self, owner):
-        return self._set_list_field("settings", owner)
+    def set_settings(self, settings):
+        """
+        Sets platform's settings.
+        @param settings: A list of settings
+        @type settings: list of L{Setting}
+        """
+        return self._set_list_field("settings", settings)
 
     def add_setting(self, setting):
+        """
+        Adds a setting to platform.
+        @param setting: A setting
+        @type setting: L{Setting}
+        """
         self._add_to_list_field("settings", setting)
 
     def get_version(self):
-        return self._get_field("version")
+        """
+        Provides platform's version number.
+        @return: A version number
+        @rtype: Integer
+        """
+        return int(self._get_field("version"))
 
     def _show(self, indent = 0):
         super(Platform, self)._show(indent)
