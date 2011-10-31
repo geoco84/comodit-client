2d83a998708b43328ff63c2c854f04d5b2a00d00
gdethier <gerard.dethier@guardis.com>
2011-10-31 15:21:18 +0100

    Documented user module.

--- cortex_client/api/user.py
+++ cortex_client/api/user.py
@@ -1,8 +1,25 @@
+# coding: utf-8
+"""
+User module.
+
+@organization: Guardis
+@copyright: 2011 Guardis SPRL, Liège, Belgium.
+"""
+
 from resource import Resource
 from cortex_client.util.json_wrapper import StringFactory
 
 class User(Resource):
+    """
+    A user's representation.
+    """
     def __init__(self, api = None, json_data = None):
+        """
+        @param api: An access point.
+        @type api: L{CortexApi}
+        @param json_data: A quasi-JSON representation of application's state.
+        @type json_data: dict, list or String
+        """
         super(User, self).__init__(json_data)
         if(api):
             self.set_api(api)
@@ -24,9 +41,19 @@ class User(Resource):
         return self._set_field("username", username)
 
     def get_roles(self):
+        """
+        Provides the list of roles associated to this user.
+        @return: A list of roles
+        @rtype: list of String
+        """
         return self._get_list_field("roles", StringFactory())
 
     def set_roles(self, roles):
+        """
+        Sets the list of roles associated to this user.
+        @param roles: A list of roles
+        @type roles: list of String
+        """
         return self._set_list_field("roles", roles)
 
     def _show(self, indent = 0):
