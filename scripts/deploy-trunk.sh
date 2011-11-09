f037582c943012dd041cce8a704c530f1ab5c66f
gdethier <gerard.dethier@guardis.com>
2011-11-09 08:56:37 +0100

    Updated RPM deploy script.

--- scripts/deploy-trunk.sh
+++ scripts/deploy-trunk.sh
@@ -13,17 +13,17 @@ RELEASE=`git describe --long --match "release*" | awk -F"-" '{print $3}'`
 
 ./scripts/build-rpm.sh
 
-cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-trunk/centos/6.0/i686/
-cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-trunk/centos/6.0/x86_64/
-cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-trunk/centos/6.0/SRPMS/
+cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/centos/6.0/i686/
+cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/centos/6.0/x86_64/
+cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-dev/centos/6.0/SRPMS/
 
-cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-trunk/fedora/14/i686/
-cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-trunk/fedora/14/x86_64/
-cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-trunk/fedora/14/SRPMS/
+cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/14/i686/
+cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/14/x86_64/
+cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-dev/fedora/14/SRPMS/
 
-cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-trunk/fedora/15/i686/
-cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-trunk/fedora/15/x86_64/
-cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-trunk/fedora/15/SRPMS/
+cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/15/i686/
+cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/15/x86_64/
+cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-dev/fedora/15/SRPMS/
 
 /var/www/html/public/updaterepo.sh
 
