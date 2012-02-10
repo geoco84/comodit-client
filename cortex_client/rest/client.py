103509762561d84e99eb18022aab2bcbe79fc392
gdethier <gerard.dethier@guardis.com>
2012-02-10 14:02:36 +0100

    Added rest.client unit tests.

--- cortex_client/rest/client.py
+++ cortex_client/rest/client.py
@@ -7,7 +7,7 @@
 # This software cannot be used and/or distributed without prior
 # authorization from Guardis.
 
-import urllib, urllib2, json, urlparse
+import urllib, urllib2, json
 import cortex_client.util.fileupload as fileupload
 from urllib2 import HTTPError
 from cortex_client.util import urllibx
@@ -22,33 +22,28 @@ class Client:
         self.password = password
 
     def create(self, resource, item = None, parameters = {}, decode = True):
-        url = self.endpoint + "/" + urllib.quote(resource)
-        if len(parameters) > 0:
-            url = url + "?" + urllib.urlencode(parameters)
+        url = self._encode_url(resource, parameters)
 
-        if item:
+        if item is not None:
             try:
                 json_data = json.dumps(item)
             except Exception, e:
                 raise ApiException("Could not encode given data: " + e.message, 0)
-            req = urllibx.RequestWithMethod(url, method = "POST", headers = self._headers(), data = json_data)
+            req = self._new_request_with_data(url, "POST", json_data)
         else:
-            req = urllibx.RequestWithMethod(url, method = "POST", headers = self._headers())
+            req = self._new_request(url, "POST")
         raw = self._urlopen(req)
         if decode:
             try:
                 return json.load(raw)
             except:
-                raise ApiException("Could not decode response: " + raw, 0)
+                raise ApiException("Could not decode response: " + raw.read(), 0)
         else:
             return raw
 
     def read(self, resource, parameters = {}, decode = True):
-        url = self.endpoint + "/" + urllib.quote(resource)
-        if len(parameters) > 0:
-            url = url + "?" + urllib.urlencode(parameters)
-
-        req = urllibx.RequestWithMethod(url, method = "GET", headers = self._headers())
+        url = self._encode_url(resource, parameters)
+        req = self._new_request(url, "GET")
         raw = self._urlopen(req)
         if decode:
             return json.load(raw)
@@ -56,12 +51,10 @@ class Client:
             return raw
 
     def update(self, resource, item = None, parameters = {}, decode = True):
-        url = self.endpoint + "/" + urllib.quote(resource)
-        if len(parameters) > 0:
-            url = url + "?" + urllib.urlencode(parameters)
-
-        req = urllibx.RequestWithMethod(url, method = "PUT", headers = self._headers())
-        if item: req.add_data(json.dumps(item))
+        url = self._encode_url(resource, parameters)
+        req = self._new_request(url, "PUT")
+        if item:
+            req.add_data(json.dumps(item))
         raw = self._urlopen(req)
         if decode:
             return json.load(raw)
@@ -69,29 +62,32 @@ class Client:
             return raw
 
     def delete(self, resource, parameters = {}):
+        url = self._encode_url(resource, parameters)
+        req = self._new_request(url, "DELETE")
+        self._urlopen(req)
+
+    def upload_to_exising_file_with_path(self, file_name, path):
+        url = self._encode_url(path, parameters = {})
+        response = fileupload.post_multipart(url.netloc, url.path, [],
+                                             [("file", file_name)],
+                                             {"Authorization": self._get_basic_authorization_field()})
+        return response
+
+    def _encode_url(self, resource, parameters):
         url = self.endpoint + "/" + urllib.quote(resource)
         if len(parameters) > 0:
             url = url + "?" + urllib.urlencode(parameters)
-
-        req = urllibx.RequestWithMethod(url, method = 'DELETE', headers = self._headers())
-        self._urlopen(req)
-        return
-
-    def upload_to_exising_file_with_path(self, file_name, path):
-        url = urlparse.urlparse(self.endpoint + "/" + urllib.quote(path))
-        response = fileupload.post_multipart(url.netloc, url.path,
-                                             [("test", "none")],
-                                             [("file", file_name)],
-                                             {"Authorization": "Basic " + (self.username + ":" + self.password).encode("base64").rstrip()})
-        return response
+        return url
 
     def _headers(self):
-        s = self.username + ":" + self.password
-        headers = {
-                   "Authorization": "Basic " + s.encode("base64").rstrip(),
+        return {
+                   "Authorization": self._get_basic_authorization_field(),
                    "Content-Type": "application/json",
-                   }
-        return headers
+                }
+
+    def _get_basic_authorization_field(self):
+        s = self.username + ":" + self.password
+        return "Basic " + s.encode("base64").rstrip()
 
     def _urlopen(self, request):
         try:
@@ -113,3 +109,8 @@ class Client:
 
             raise ApiException(message, err.code)
 
+    def _new_request(self, url, m):
+        return urllibx.RequestWithMethod(url, method = m, headers = self._headers())
+
+    def _new_request_with_data(self, url, m, d):
+        return urllibx.RequestWithMethod(url, method = m, headers = self._headers(), data = d)
---
+++ test/rest/clienttests.py
@@ -0,0 +1,219 @@
+import unittest, json
+
+from cortex_client.rest.client import Client
+from cortex_client.rest.exceptions import ApiException
+
+class RequestWithMethodMock(object):
+    def __init__(self, url, method, headers = {}, data = None):
+        self._url = url
+
+    def get_url(self):
+        return self._url
+
+    def add_data(self, obj):
+        pass
+
+class RequestResult(object):
+    def __init__(self, string_result):
+        self._string_result = string_result
+
+    def __str__(self):
+        return self._string_result
+
+    def read(self):
+        return self._string_result
+
+class ClientTest(unittest.TestCase):
+    def setUp(self):
+        self._path = "path"
+        self._params = ""
+        self._endpoint = "http://localhost/api"
+        self._user = "user"
+        self._pass = "pass"
+        self._headers = None
+        self._urlopen_result = None
+
+        self._client = Client(self._endpoint, self._user, self._pass)
+
+        # Mock some Client methods
+        self._client._new_request = self._new_request
+        self._client._new_request_with_data = self._new_request_with_data
+
+    def tearDown(self):
+        pass
+
+    # Create tests
+
+    def test_create_success(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        data = {"test":"value"}
+        self._urlopen_result = json.dumps(data)
+
+        result = self._client.create(self._path, item = data)
+        self.assertEqual(data, result, "Wrong result returned")
+
+    def test_create_success_w_params(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        data = {"test":"value"}
+        self._urlopen_result = json.dumps(data)
+        self._params = "?param2=value2&param1=value1"
+
+        result = self._client.create(self._path, item = data, parameters = {"param1":"value1", "param2":"value2"})
+        self.assertEqual(data, result, "Wrong result returned")
+
+    def test_create_wrong_url(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        data = {"test":"value"}
+        self._urlopen_result = json.dumps(data)
+
+        try:
+            self._client.create(self._path + "x", item = data)
+        except:
+            return
+        self.assertFalse(True)
+
+    def test_create_success_wo_decode(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        data = {"test":"value"}
+        self._urlopen_result = json.dumps(data)
+
+        result = self._client.create(self._path, item = data, parameters = {}, decode = False)
+        self.assertEqual(data, json.load(result), "Wrong result returned")
+
+    def test_create_failure_urlopen(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_failure
+
+        try:
+            self._client.create(self._path, item = {})
+        except ApiException:
+            return
+        self.assertFalse(True)
+
+
+    # Delete tests
+
+    def test_delete_success(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        self._client.delete(self._path)
+
+    def test_delete_wrong_url(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        try:
+            self._client.delete(self._path + "x")
+        except:
+            return
+        self.assertFalse(True)
+
+    def test_delete_failure_urlopen(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_failure
+
+        try:
+            self._client.delete(self._path)
+        except ApiException:
+            return
+        self.assertFalse(True)
+
+
+    # Read tests
+
+    def test_read_success(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        data = {"test":"value"}
+        self._urlopen_result = json.dumps(data)
+
+        result = self._client.read(self._path)
+        self.assertEqual(data, result, "Wrong result returned")
+
+    def test_read_wrong_url(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        try:
+            self._client.read(self._path + "x")
+        except:
+            return
+        self.assertFalse(True)
+
+    def test_read_failure_urlopen(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_failure
+
+        try:
+            self._client.read(self._path)
+        except ApiException:
+            return
+        self.assertFalse(True)
+
+
+    # Update tests
+
+    def test_update_success(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        data = {"test":"value"}
+        self._urlopen_result = json.dumps(data)
+
+        result = self._client.update(self._path, item = data)
+        self.assertEqual(data, result, "Wrong result returned")
+
+    def test_update_wrong_url(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        data = {"test":"value"}
+        self._urlopen_result = json.dumps(data)
+
+        try:
+            self._client.update(self._path + "x", item = data)
+        except:
+            return
+        self.assertFalse(True)
+
+    def test_update_success_wo_decode(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_success
+        data = {"test":"value"}
+        self._urlopen_result = json.dumps(data)
+
+        result = self._client.update(self._path, item = data, parameters = {}, decode = False)
+        self.assertEqual(data, json.load(result), "Wrong result returned")
+
+    def test_update_failure_urlopen(self):
+        # Mock _urlopen
+        self._client._urlopen = self._urlopen_failure
+
+        try:
+            self._client.update(self._path, item = {})
+        except ApiException:
+            return
+        self.assertFalse(True)
+
+
+    # Helpers
+
+    def _new_request(self, url, m):
+        req = RequestWithMethodMock(url, method = m, headers = self._headers)
+        return req
+
+    def _new_request_with_data(self, url, m, d):
+        req = RequestWithMethodMock(url, method = m, headers = self._headers, data = d)
+        return req
+
+    def _urlopen_success(self, request):
+        if request.get_url() == self._endpoint + "/" + self._path + self._params:
+            return RequestResult(self._urlopen_result)
+        else:
+            raise Exception()
+
+    def _urlopen_failure(self, request):
+        raise ApiException("message", 404)
+
+if __name__ == '__main__':
+    unittest.main()
