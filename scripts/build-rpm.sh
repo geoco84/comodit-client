9d977d9ac1964ebb4732e7e9b73ed455ceb6074d
gdethier <gerard.dethier@guardis.com>
2012-02-24 15:42:36 +0100

    Workaround for old publican on devel.bruxelles

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -5,13 +5,22 @@ RELEASE=`git describe --long --match "release*" | awk -F"-" '{print $3}'`
 COMMIT=`git describe --long --match "release*" | awk -F"-" '{print $4}'`
 
 NAME=cortex-client
-TAR_CONTENT="cortex_client templates conf setup.py cortex scripts/completions.sh cortex-doc/tmp/en-US/man/cortex.1"
+MAN_PAGE_FILE="cortex-doc/tmp/en-US/man/cortex.1"
+TAR_CONTENT="cortex_client templates conf setup.py cortex scripts/completions.sh "${MAN_PAGE_FILE}
 PLATFORMS=(epel-6-i386 fedora-15-i386 fedora-16-i386)
 
 cd `dirname $0`
 cd ..
 
-scripts/build-man.sh
+#scripts/build-man.sh
+
+# Workaround for lack of proper publican version on devel.bruxelles
+if [[ ! -f ${MAN_PAGE_FILE} ]]
+then
+    echo "!!! Man page could not be generated, creating dummy file !!!"
+    mkdir -p `dirname ${MAN_PAGE_FILE}`
+    touch ${MAN_PAGE_FILE}
+fi
 
 sed "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${NAME}.spec.template > rpmbuild/SPECS/${NAME}.spec
 sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${NAME}.spec
