ab14525d413073a8fbc81ce2d6d0b3b2f4182025
gdethier <gerard.dethier@guardis.com>
2012-02-24 16:27:48 +0100

    Fixed deploy-trunk (removed fedora 14 / added fedora 16 support)

--- scripts/deploy-trunk.sh
+++ scripts/deploy-trunk.sh
@@ -19,13 +19,13 @@ cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /v
 cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/centos/6.0/x86_64/
 cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-dev/centos/6.0/SRPMS/
 
-cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/14/i686/
-cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/14/x86_64/
-cp /var/lib/mock/fedora-14-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-dev/fedora/14/SRPMS/
-
 cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/15/i686/
 cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/15/x86_64/
 cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-dev/fedora/15/SRPMS/
+
+cp /var/lib/mock/fedora-16-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/16/i686/
+cp /var/lib/mock/fedora-16-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/16/x86_64/
+cp /var/lib/mock/fedora-16-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-dev/fedora/16/SRPMS/
 
 /var/www/html/public/updaterepo.sh
 
