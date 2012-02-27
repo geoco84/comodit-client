59efa0c31b44af52df31af7353f91536ca0a6f0d
gdethier <gerard.dethier@guardis.com>
2012-02-27 08:53:24 +0100

    If man page could not be generated, use a given fallback file instead of an empty one. Closes #36

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -6,20 +6,21 @@ COMMIT=`git describe --long --match "release*" | awk -F"-" '{print $4}'`
 
 NAME=cortex-client
 MAN_PAGE_FILE="cortex-doc/tmp/en-US/man/cortex.1"
+FALLBACK_MAN=`readlink -f scripts/cortex.1`
 TAR_CONTENT="cortex_client templates conf setup.py cortex scripts/completions.sh "${MAN_PAGE_FILE}
 PLATFORMS=(epel-6-i386 fedora-15-i386 fedora-16-i386)
 
 cd `dirname $0`
 cd ..
 
-#scripts/build-man.sh
+scripts/build-man.sh
 
 # Workaround for lack of proper publican version on devel.bruxelles
 if [[ ! -f ${MAN_PAGE_FILE} ]]
 then
-    echo "!!! Man page could not be generated, creating dummy file !!!"
+    echo "!!! Man page could not be generated, using fallback file !!!"
     mkdir -p `dirname ${MAN_PAGE_FILE}`
-    touch ${MAN_PAGE_FILE}
+    cp ${FALLBACK_MAN} ${MAN_PAGE_FILE}
 fi
 
 sed "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${NAME}.spec.template > rpmbuild/SPECS/${NAME}.spec
