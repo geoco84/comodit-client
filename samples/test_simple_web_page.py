a749a9f3cba669726f43218872bc0025c2f2fdf9
gdethier <gerard.dethier@guardis.com>
2012-04-20 16:03:13 +0200

    Added time-out to test_simple_web_page.

--- samples/test_simple_web_page.py
+++ samples/test_simple_web_page.py
@@ -17,8 +17,8 @@ from cortex_client.api.exceptions import PythonApiException
 #==============================================================================
 # Script
 
-def test_web_server_page(host, port, page):
-    wait_changes(host)
+def test_web_server_page(host, port, page, time_out = 0):
+    wait_changes(host, time_out)
     ip = host.instance().get_single_resource().get_ip("eth0")
     try:
         return urllib2.urlopen("http://" + ip + ":" + str(port) + page)
@@ -27,7 +27,7 @@ def test_web_server_page(host, port, page):
     except urllib2.URLError, e:
         raise Exception("Unexpected URL error: " + str(e.reason))
 
-def install_simple_web_page(host, settings):
+def install_simple_web_page(host, settings, time_out = 0):
     print "Installing simple web page..."
     context = host.applications()._new_resource()
     context.set_application(test_setup.global_vars.simple_web_page_name)
@@ -41,26 +41,32 @@ def install_simple_web_page(host, settings):
             context.add_simple_setting(s["key"], s["value"])
 
     host.applications().add_resource(context)
-    wait_changes(host)
+    wait_changes(host, time_out)
     print "Simple web page successfully installed."
 
-def uninstall_simple_web_page(host):
+def uninstall_simple_web_page(host, time_out = 0):
     print "Uninstalling simple web page..."
     try:
         host.applications().get_resource(test_setup.global_vars.simple_web_page_name).delete()
-        wait_changes(host)
+        wait_changes(host, time_out)
     except PythonApiException, e:
         print e.message
     print "Simple web page successfully installed."
 
-def check_page_content(host, port, page, value):
-    content_reader = test_web_server_page(host, port, page)
+def check_page_content(host, port, page, value, time_out = 0):
+    content_reader = test_web_server_page(host, port, page, time_out)
     content = content_reader.read()
     if content.find(value) == -1:
         raise Exception("Unexpected page content")
 
 def get_simple_setting_json(key, value):
     return {"key": key, "value": value}
+
+def get_time_out(argv):
+    if len(argv) > 1:
+        return int(argv[1])
+    else:
+        return 0
 
 def setup(argv):
     api = CortexApi(test_setup.global_vars.comodit_url, test_setup.global_vars.comodit_user, test_setup.global_vars.comodit_pass)
@@ -69,8 +75,10 @@ def setup(argv):
     env = org.environments().get_resource(test_setup.global_vars.env_name)
     host = env.hosts().get_resource(argv[0])
 
-    install_web_server(host, [get_simple_setting_json("httpd_port", "80")])
-    install_simple_web_page(host, [get_simple_setting_json("simple_web_page", "hello")])
+    time_out = get_time_out(argv)
+
+    install_web_server(host, [get_simple_setting_json("httpd_port", "80")], time_out)
+    install_simple_web_page(host, [get_simple_setting_json("simple_web_page", "hello")], time_out)
 
 def run(argv):
     api = CortexApi(test_setup.global_vars.comodit_url, test_setup.global_vars.comodit_user, test_setup.global_vars.comodit_pass)
@@ -79,7 +87,9 @@ def run(argv):
     env = org.environments().get_resource(test_setup.global_vars.env_name)
     host = env.hosts().get_resource(argv[0])
 
-    check_page_content(host, 80, "/index.html", "Setting value: hello")
+    time_out = get_time_out(argv)
+
+    check_page_content(host, 80, "/index.html", "Setting value: hello", time_out)
 
 def tear_down(argv):
     api = CortexApi(test_setup.global_vars.comodit_url, test_setup.global_vars.comodit_user, test_setup.global_vars.comodit_pass)
@@ -88,8 +98,10 @@ def tear_down(argv):
     env = org.environments().get_resource(test_setup.global_vars.env_name)
     host = env.hosts().get_resource(argv[0])
 
-    uninstall_web_server(host)
-    uninstall_simple_web_page(host)
+    time_out = get_time_out(argv)
+
+    uninstall_web_server(host, time_out)
+    uninstall_simple_web_page(host, time_out)
 
 #==============================================================================
 # Entry point
