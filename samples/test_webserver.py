097c902b7582930c326715a6fabd36e666daac6e
gdethier <gerard.dethier@guardis.com>
2012-04-20 16:02:57 +0200

    Added time-out to test_webserver.

--- samples/test_webserver.py
+++ samples/test_webserver.py
@@ -1,14 +1,11 @@
 # Setup Python path
-import sys, urllib2, test_utils
-import definitions as defs
+import sys, urllib2, test_utils, time
 import setup as test_setup
 sys.path.append("..")
 
 
 #==============================================================================
 # Imports section
-
-import time
 
 from cortex_client.api.api import CortexApi
 from cortex_client.api.exceptions import PythonApiException
@@ -17,12 +14,17 @@ from cortex_client.api.exceptions import PythonApiException
 #==============================================================================
 # Script
 
-def wait_changes(host):
+def wait_changes(host, time_out = 0):
+    start_time = time.time()
     while len(host.get_changes()) > 0:
         time.sleep(3)
+        now = time.time()
 
-def test_web_server_default(host, port):
-    wait_changes(host)
+        if time_out > 0 and (now - start_time) > time_out:
+            raise Exception("Time-out while waiting for changes")
+
+def test_web_server_default(host, port, time_out = 0):
+    wait_changes(host, time_out)
     ip = host.instance().get_single_resource().get_ip("eth0")
     try:
         urllib2.urlopen("http://" + ip + ":" + str(port))
@@ -30,7 +32,7 @@ def test_web_server_default(host, port):
         if e.code != 403:
             raise Exception("Unexpected HTTP error: " + str(e.code))
 
-def install_web_server(host, settings = []):
+def install_web_server(host, settings = [], time_out = 0):
     print "Installing web server..."
     context = host.applications()._new_resource()
     context.set_application(test_setup.global_vars.web_server_name)
@@ -44,17 +46,23 @@ def install_web_server(host, settings = []):
             context.add_simple_setting(s["key"], s["value"])
 
     host.applications().add_resource(context)
-    wait_changes(host)
+    wait_changes(host, time_out)
     print "Web server successfully installed."
 
-def uninstall_web_server(host):
+def uninstall_web_server(host, time_out = 0):
     print "Uninstalling web server..."
     try:
         host.applications().get_resource(test_setup.global_vars.web_server_name).delete()
-        wait_changes(host)
+        wait_changes(host, time_out)
     except PythonApiException, e:
         print e.message
     print "Web server successfully uninstalled."
+
+def get_time_out(argv):
+    if len(argv) > 1:
+        return int(argv[1])
+    else:
+        return 0
 
 def setup(argv):
     api = CortexApi(test_setup.global_vars.comodit_url, test_setup.global_vars.comodit_user, test_setup.global_vars.comodit_pass)
@@ -63,7 +71,9 @@ def setup(argv):
     env = org.environments().get_resource(test_setup.global_vars.env_name)
     host = env.hosts().get_resource(argv[0])
 
-    install_web_server(host)
+    time_out = get_time_out(argv)
+
+    install_web_server(host, time_out = time_out)
 
 def run(argv):
     api = CortexApi(test_setup.global_vars.comodit_url, test_setup.global_vars.comodit_user, test_setup.global_vars.comodit_pass)
@@ -72,7 +82,9 @@ def run(argv):
     env = org.environments().get_resource(test_setup.global_vars.env_name)
     host = env.hosts().get_resource(argv[0])
 
-    test_web_server_default(host, 80)
+    time_out = get_time_out(argv)
+
+    test_web_server_default(host, 80, time_out)
 
 def tear_down(argv):
     api = CortexApi(test_setup.global_vars.comodit_url, test_setup.global_vars.comodit_user, test_setup.global_vars.comodit_pass)
@@ -81,7 +93,9 @@ def tear_down(argv):
     env = org.environments().get_resource(test_setup.global_vars.env_name)
     host = env.hosts().get_resource(argv[0])
 
-    uninstall_web_server(host)
+    time_out = get_time_out(argv)
+
+    uninstall_web_server(host, time_out)
 
 #==============================================================================
 # Entry point
