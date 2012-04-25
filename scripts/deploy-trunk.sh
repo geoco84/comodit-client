d44b219a4e453bec741720ba6bd98d49b6cc2606
gdethier <gerard.dethier@guardis.com>
2012-04-25 13:15:53 +0200

    Updated deployment script.

--- scripts/deploy-trunk.sh
+++ scripts/deploy-trunk.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 echo "Building cortex-client from master"
 
+set -e
+
 cd `dirname $0`
 cd ..
 
 git checkout master
 git pull
-git submodule init
-git submodule update
 
 NAME="cortex-client"
-VERSION=`git describe --long --match "release*" | awk -F"-" '{print $2}'`
-RELEASE=`git describe --long --match "release*" | awk -F"-" '{print $3}'`
+VERSION=`git describe --long --match "release*dev" | awk -F"-" '{print $2}'`
+RELEASE=`git describe --long --match "release*dev" | awk -F"-" '{print $3}'`
 
 ./scripts/build-rpm.sh
-
-cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/centos/6.0/i686/
-cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/centos/6.0/x86_64/
-cp /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-dev/centos/6.0/SRPMS/
-
-cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/15/i686/
-cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/15/x86_64/
-cp /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-dev/fedora/15/SRPMS/
-
-cp /var/lib/mock/fedora-16-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/16/i686/
-cp /var/lib/mock/fedora-16-i386/result/${NAME}-${VERSION}-${RELEASE}*.noarch.rpm /var/www/html/public/comodit-dev/fedora/16/x86_64/
-cp /var/lib/mock/fedora-16-i386/result/${NAME}-${VERSION}-${RELEASE}*.src.rpm /var/www/html/public/comodit-dev/fedora/16/SRPMS/
-
-/var/www/html/public/updaterepo.sh
-
+deploy-trunk $HOME/rpmbuild/RPMS/noarch/${NAME}-${VERSION}-${RELEASE}.noarch.rpm
+updaterepo
