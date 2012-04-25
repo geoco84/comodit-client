be8b614580ed68c7ff6c39ba6a34a2e92717a8d9
gdethier <gerard.dethier@guardis.com>
2012-04-25 13:39:29 +0200

    Fixed deployment script.

--- scripts/deploy-trunk.sh
+++ scripts/deploy-trunk.sh
@@ -14,5 +14,7 @@ VERSION=`git describe --long --match "release*dev" | awk -F"-" '{print $2}'`
 RELEASE=`git describe --long --match "release*dev" | awk -F"-" '{print $3}'`
 
 ./scripts/build-rpm.sh
-deploy-trunk $HOME/rpmbuild/RPMS/noarch/${NAME}-${VERSION}-${RELEASE}.noarch.rpm
+deploy-trunk /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}.noarch.rpm
+deploy-trunk /var/lib/mock/fedora-15-i386/result/${NAME}-${VERSION}-${RELEASE}.noarch.rpm
+deploy-trunk /var/lib/mock/fedora-16-i386/result/${NAME}-${VERSION}-${RELEASE}.noarch.rpm
 updaterepo
