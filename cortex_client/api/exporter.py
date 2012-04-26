f578810ac7253220f27ec1d791a281bfaacf72cd
gdethier <gerard.dethier@guardis.com>
2012-04-26 11:03:04 +0200

    Added ability to export platforms and distributions.

--- cortex_client/api/exporter.py
+++ cortex_client/api/exporter.py
@@ -17,14 +17,23 @@ class Export(object):
             with open(os.path.join(output_folder, file_name), "w") as f:
                 f.write(template.get_content().read())
 
-    def export_application(self, app, app_folder):
+    def _export_resource_with_files(self, res, res_folder):
         # Ensures local repository does not contain stale data
-        if(os.path.exists(app_folder) and len(os.listdir(app_folder)) > 0) and not self._force:
-            raise ExportException(app_folder + " already exists and is not empty.")
+        if(os.path.exists(res_folder) and len(os.listdir(res_folder)) > 0) and not self._force:
+            raise ExportException(res_folder + " already exists and is not empty.")
 
-        app.dump(app_folder)
+        res.dump(res_folder)
 
         # Dump files' content to disk
-        files_folder = os.path.join(app_folder, "files")
+        files_folder = os.path.join(res_folder, "files")
         path.ensure(files_folder)
-        self._export_files_content(app, files_folder)
+        self._export_files_content(res, files_folder)
+
+    def export_application(self, app, app_folder):
+        self._export_resource_with_files(app, app_folder)
+
+    def export_distribution(self, dist, dist_folder):
+        self._export_resource_with_files(dist, dist_folder)
+
+    def export_platform(self, plat, plat_folder):
+        self._export_resource_with_files(plat, plat_folder)
--- cortex_client/control/distributions.py
+++ cortex_client/control/distributions.py
@@ -13,6 +13,9 @@ from cortex_client.control.organization_resource import OrganizationResourceCont
 from cortex_client.control.settings import DistributionSettingsController
 from cortex_client.control.files import DistributionFilesController
 from cortex_client.control.parameters import DistributionParametersController
+from cortex_client.api.exporter import Export
+from cortex_client.util import globals
+from cortex_client.control.doc import ActionDoc
 
 class DistributionsController(OrganizationResourceController):
 
@@ -28,6 +31,11 @@ class DistributionsController(OrganizationResourceController):
 
         self._doc = "Distributions handling."
 
+        # actions
+        self._register(["export"], self._export, self._print_export_completions)
+
+        self._register_action_doc(self._export_doc())
+
     def _get_collection(self, org_name):
         return collections.distributions(self._api, org_name)
 
@@ -37,4 +45,27 @@ class DistributionsController(OrganizationResourceController):
         json_wrapper._del_field("settings")
         json_wrapper._del_field("files")
         json_wrapper._del_field("parameters")
+
+    def _print_export_completions(self, param_num, argv):
+        if param_num < 2:
+            self._print_resource_completions(param_num, argv)
+        elif param_num == 2:
+            self._print_dir_completions()
+
+    def _export(self, argv):
+        self._options = globals.options
+
+        app = self._get_resource(argv)
+
+        root_folder = app.get_name()
+        if len(argv) > 2:
+            root_folder = argv[2]
+
+        export = Export(globals.options)
+        export.export_application(app, root_folder)
+
+    def _export_doc(self):
+        return ActionDoc("export", "<org_name> <app_name> [<output_folder>] [--force]", """
+        Export application onto disk. --force option causes existing files to
+        be overwritten.""")
 
