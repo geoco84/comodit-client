d879ef2e7da69df9bd1322c8482147a5da0c0454
gdethier <gerard.dethier@guardis.com>
2012-05-03 09:47:20 +0200

    Added delete-all action to compliance controller.

--- cortex_client/control/compliance.py
+++ cortex_client/control/compliance.py
@@ -10,6 +10,7 @@
 from cortex_client.api import collections
 from cortex_client.control.exceptions import ArgumentException
 from cortex_client.control.resource import ResourceController
+from cortex_client.control.doc import ActionDoc
 
 class ComplianceController(ResourceController):
 
@@ -19,6 +20,10 @@ class ComplianceController(ResourceController):
 
         self._unregister("add")
         self._unregister("update")
+
+        self._register(["delete-all"], self._delete_all, self._print_collection_completions)
+
+        self._register_action_doc(self._delete_all_doc())
 
     def get_collection(self, argv):
         if len(argv) < 3:
@@ -45,3 +50,11 @@ class ComplianceController(ResourceController):
         if len(argv) < 4:
             raise ArgumentException("An organization, environment, host and compliance error name must be provided");
         return argv[3]
+
+    def _delete_all(self, argv):
+        compliance = self.get_collection(argv)
+        compliance.clear_collection()
+
+    def _delete_all_doc(self):
+        return ActionDoc("delete-all", "<org_name> <env_name> <host_name>", """
+        Deletes all compliance errors.""")
