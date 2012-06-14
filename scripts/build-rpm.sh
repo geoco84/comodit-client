11f9ffcd58b240862131d47a805f87ff4aaea189
gdethier <gerard.dethier@guardis.com>
2012-06-14 12:54:52 +0200

    Fixed RPM build.

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -3,21 +3,21 @@
 NAME="cortex-client"
 SPEC_FILE_NAME="cortex-client"
 PLATFORMS="epel-6-i386 fedora-15-i386 fedora-16-i386"
-TAR_CONTENT="cortex_client templates conf setup.py cortex auto_completion/cortex doc/cortex.1"
+TAR_CONTENT="cortex_client templates conf setup.py cortex auto_completion/cortex doc/cortex.1 README"
 
 cd `dirname $0`
 cd ..
 
+# Set version information
 . scripts/build-pkg-functions
-
 set_version $1 $2
 generate_version_file $VERSION $RELEASE
+
+tar -cvzf $HOME/rpmbuild/SOURCES/${NAME}-${VERSION}-${RELEASE}.tar.gz ${TAR_CONTENT}
 
 sed "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${SPEC_FILE_NAME}.spec.template > rpmbuild/SPECS/${SPEC_FILE_NAME}.spec
 sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${SPEC_FILE_NAME}.spec
 sed -i "s/#COMMIT#/${COMMIT}/g" rpmbuild/SPECS/${SPEC_FILE_NAME}.spec
-
-tar -cvzf $HOME/rpmbuild/SOURCES/${NAME}-${VERSION}-${RELEASE}.tar.gz ${TAR_CONTENT}
 rpmbuild -ba rpmbuild/SPECS/${SPEC_FILE_NAME}.spec
 
 for platform in $PLATFORMS
