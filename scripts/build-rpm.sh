e654b1fadbb356d6dea233b0e61b783895ed4cd0
Maxime Bourdouxhe <bourdouxhemaxime@gmail.com>
2016-03-23 09:18:35 +0100

    Add new platforms (fedora22-23)

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -2,7 +2,7 @@
 
 NAME="comodit-client"
 SPEC_FILE_NAME="comodit-client"
-PLATFORMS="epel-6-i386 fedora-16-i386 fedora-17-i386 fedora-18-i386"
+PLATFORMS=(epel-6-x86_64 epel-7-x86_64 fedora-21-x86_64 fedora-22-x86_64 fedora-23-x86_64)
 TAR_CONTENT="comodit_client templates setup.py comodit auto_completion/comodit README.md doc/comodit.1 rpmbuild/etc"
 
 cd `dirname $0`
