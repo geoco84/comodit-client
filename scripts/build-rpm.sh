2d0b73476d0e139d263792078361338ff6b93b22
Maxime Bourdouxhe <maxime.bourdouxhe@seaters.com>
2016-07-27 15:39:19 +0200

    Add fedora 23-24

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -2,8 +2,11 @@
 
 NAME="comodit-client"
 SPEC_FILE_NAME="comodit-client"
-PLATFORMS=(epel-6-x86_64 epel-7-x86_64 fedora-21-x86_64 fedora-22-x86_64 fedora-23-x86_64)
+PLATFORMS=(epel-6-i386 epel-6-x86_64 epel-7-x86_64 fedora-22-x86_64 fedora-23-x86_64 fedora-24-x86_64)
 TAR_CONTENT="comodit_client templates setup.py comodit auto_completion/comodit README.md doc/comodit.1 rpmbuild/etc"
+
+# Create build structure
+rpmdev-setuptree
 
 cd `dirname $0`
 cd ..
@@ -18,6 +21,7 @@ tar -cvzf $HOME/rpmbuild/SOURCES/${NAME}-${VERSION}-${RELEASE}.tar.gz ${TAR_CONT
 sed "s/#VERSION#/${VERSION}/g" rpmbuild/SPECS/${SPEC_FILE_NAME}.spec.template > rpmbuild/SPECS/${SPEC_FILE_NAME}.spec
 sed -i "s/#RELEASE#/${RELEASE}/g" rpmbuild/SPECS/${SPEC_FILE_NAME}.spec
 sed -i "s/#COMMIT#/${COMMIT}/g" rpmbuild/SPECS/${SPEC_FILE_NAME}.spec
+
 rpmbuild -ba rpmbuild/SPECS/${SPEC_FILE_NAME}.spec
 
 if [ -f "/usr/bin/mock" ]
@@ -25,5 +29,7 @@ then
 for platform in "${PLATFORMS[@]}"
 do
     /usr/bin/mock -r ${platform} --rebuild $HOME/rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
+    mkdir -p $HOME/packages/${platform}
+    cp -f /var/lib/mock/${platform}/result/*.rpm $HOME/packages/${platform}
 done
 fi
