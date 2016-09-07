50188318d8e466b4de019028c51916280ae97e6a
Daniel BARTZ <daniel.bartz@guardis.com>
2016-09-07 18:26:19 +0200

    Add test console

--- scripts/deploy-trunk.sh
+++ scripts/deploy-trunk.sh
@@ -17,4 +17,5 @@ RELEASE=`git describe --long --match "release*dev" | awk -F"-" '{print $3}'`
 deploy-trunk /var/lib/mock/epel-6-i386/result/${NAME}-${VERSION}-${RELEASE}.el6.noarch.rpm /public/centos
 deploy-trunk /var/lib/mock/fedora-16-i386/result/${NAME}-${VERSION}-${RELEASE}.fc16.noarch.rpm /public/fedora/16
 deploy-trunk /var/lib/mock/fedora-17-i386/result/${NAME}-${VERSION}-${RELEASE}.fc17.noarch.rpm /public/fedora/17
+deploy-trunk /var/lib/mock/fedora-18-i386/result/${NAME}-${VERSION}-${RELEASE}.fc18.noarch.rpm /public/fedora/18
 updaterepo
