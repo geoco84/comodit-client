2784ba1b54f561758ea30f239ceb565014074748
Maxime Bourdouxhe <bourdouxhemaxime@gmail.com>
2018-05-03 11:33:31 +0200

    Update build-dev-rpm

--- scripts/build-dev-rpm.sh
+++ scripts/build-dev-rpm.sh
@@ -3,7 +3,7 @@
 # Exit on errors
 set -e
 
-SYSTEMD_PLATFORMS=(epel-7-x86_64 fedora-25-x86_64 fedora-26-x86_64 fedora-27-x86_64)
+SYSTEMD_PLATFORMS=(epel-7-x86_64 fedora-26-x86_64 fedora-27-x86_64 fedora-28-x86_64)
 PLATFORMS=(epel-6-i386 epel-6-x86_64)
 NAME="comodit-client"
 
