261b9bfaad84f9a0dd2b1fa46f14bbac398d8e3b
maxime bourdouxhe <mbourdouxhe@willow.angleur.guardis.be>
2018-06-12 16:37:15 +0200

    Add --bootstrap-chroot when using mock

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -53,14 +53,14 @@ if [ -f "/usr/bin/mock" ]
 then
   for platform in "${PLATFORMS[@]}"
   do
-    /usr/bin/mock -r ${platform} --rebuild rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
+    /usr/bin/mock --bootstrap-chroot -r ${platform} --rebuild rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
     mkdir -p ${HOME}/packages/${platform}
     mv /var/lib/mock/${platform}/result/*.rpm ${HOME}/packages/${platform}
   done
 
   for platform in "${SYSTEMD_PLATFORMS[@]}"
   do
-    /usr/bin/mock -r ${platform} --define "use_systemd 1" --rebuild rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
+    /usr/bin/mock --bootstrap-chroot -r ${platform} --define "use_systemd 1" --rebuild rpmbuild/SRPMS/${NAME}-${VERSION}-${RELEASE}*.src.rpm
     mkdir -p ${HOME}/packages/${platform}
     mv /var/lib/mock/${platform}/result/*.rpm ${HOME}/packages/${platform}
   done
